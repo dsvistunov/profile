176f5cd7a8c6fb5f6cb6ed5a81c722c178b56ba9
dsvistunov <d.svistuno0v1991@gmail.com>
2018-07-13 14:34:57 +0300

    added fixtures, model, migration

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_birth = models.DateField(
+        null=True, blank=True
+    )
+    email = models.EmailField(max_length=30)
+    jabber = models.EmailField(max_length=30)
+    skype = models.CharField(max_length=30)
+    other_contacts = models.TextField()
+    bio = models.TextField()
