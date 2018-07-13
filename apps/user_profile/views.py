7d41022355e110406d24d4112ee3eea0bf31e77a
dsvistunov <d.svistuno0v1991@gmail.com>
2018-07-13 14:35:26 +0300

    returns model data

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,6 +1,12 @@
 from django.views.generic.base import TemplateView
+from .models import Profile
 
 
 class IndexView(TemplateView):
 	
 	template_name = 'index.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context['data'] = Profile.objects.first()
+		return context
