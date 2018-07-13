f6b4d4ee050011f709cdee83ac93dbff6be13d60
dsvistunov <d.svistuno0v1991@gmail.com>
2018-07-13 17:01:57 +0300

    changed heroku setts

--- src/settings.py
+++ src/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
